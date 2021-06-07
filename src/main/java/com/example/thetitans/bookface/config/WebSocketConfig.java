4b5dcab9085a7fd0c5e257266803692cfafc9a55
tranminhthong <minhthong9711@gmail.com>
2021-06-07 19:17:40 +0700

    deploy

--- src/main/java/com/example/thetitans/bookface/config/WebSocketConfig.java
+++ src/main/java/com/example/thetitans/bookface/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200").withSockJS();
+                .setAllowedOrigins("https://bookfaces.netlify.app").withSockJS();
     }
 
     @Override
