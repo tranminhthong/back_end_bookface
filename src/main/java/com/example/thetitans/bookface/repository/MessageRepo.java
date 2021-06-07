f72a074303c60959893b72f7a726df1206560f60
tranminhthong <minhthong9711@gmail.com>
2021-06-07 20:49:43 +0700

    deploy

--- src/main/java/com/example/thetitans/bookface/repository/MessageRepo.java
+++ src/main/java/com/example/thetitans/bookface/repository/MessageRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
-    @Query(value = "call mxh_bookface.getAllChatBetweenTwoUser(:user1Id,:user2Id,:size)", nativeQuery = true)
+    @Query(value = "select getAllChatBetweenTwoUser(:user1Id,:user2Id,:size)", nativeQuery = true)
     Iterable<Message> getAllHistoryBetweenTwoUser(Long user1Id, Long user2Id, Integer size);
 }
